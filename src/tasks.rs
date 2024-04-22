290374e636b1b86730d102e0df41e2966bafd726
Orland Contreras (Exiadev) <orland102@gmail.com>
2024-04-21 22:57:40 -0500

    Rectafor task function

--- src/tasks.rs
+++ src/tasks.rs
@@ -29,7 +29,7 @@ impl Task {
 
         // Consume the file's contents as a vector of tasks.
         let mut tasks = collect_tasks(&file)?;
-        
+
         // Write the modified task list back into the file.
         tasks.push(task);
         serde_json::to_writer(file, &tasks)?;
@@ -45,14 +45,14 @@ impl Task {
             .open(journal_path)?;
 
         // Consume the file's contents as a vector of tasks.
-        let mut tasks =  collect_tasks(&file)?;    
+        let mut tasks = collect_tasks(&file)?;
 
         // Try to remove the task.
         if task_position == 0 || task_position > tasks.len() {
             return Err(Error::new(ErrorKind::InvalidInput, "Invalid Task ID"));
         }
         tasks.remove(task_position - 1);
-        
+
         // Write the modified task list back into the file.
         file.set_len(0);
         serde_json::to_writer(file, &tasks)?;
